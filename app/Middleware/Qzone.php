373edeb2c55185d8b9a54ad2811de00eec7ee39f
Mangoa Lau <mangolau34@gmail.com>
2019-08-09 11:42:58 +0800

    更换获取说说方式，原来的不能用

--- app/Middleware/Qzone.php
+++ app/Middleware/Qzone.php
@@ -9,8 +9,9 @@ use Curl\Curl;
 
 class Qzone
 {
-	const QQ_TWITTEE_URL = 'http://taotao.qq.com/cgi-bin/emotion_cgi_homepage_msg';
-	// const QQ_TWITTEE_URL = 'http://sh.taotao.qq.com/cgi-bin/emotion_cgi_feedlist_v6?hostUin=2892391933&ftype=0&sort=0&pos=0&num=10&replynum=0&g_tk=&callbackFun=_preloadCallback&code_version=1&format=jsonp&need_private_comment=1';
+//	const QQ_TWITTEE_URL = 'http://taotao.qq.com/cgi-bin/emotion_cgi_homepage_msg';
+
+	const QQ_TWITTEE_URL = 'http://sh.taotao.qq.com/cgi-bin/emotion_cgi_feedlist_v6';// http://sh.taotao.qq.com/cgi-bin/emotion_cgi_feedlist_v6?hostUin=2892391933&ftype=0&sort=0&pos=0&num=10&replynum=0&g_tk=&callbackFun=_preloadCallback&code_version=1&format=jsonp&need_private_comment=1
 	const QQ_BLOG_URL = '';
 
 	/**
@@ -20,7 +21,7 @@ class Qzone
 	 * @param  integer $count
 	 * @return mixed
 	 */
-	public static function getQQTwittees($qq, $page = 1, $count = 10)
+	/*public static function getQQTwittees($qq, $page = 1, $count = 10)
 	{
 		$ret = [
 			'code' => 1000,
@@ -37,6 +38,7 @@ class Qzone
 
 			$url = self::QQ_TWITTEE_URL . '?' . http_build_query($params);
 			$curl = new Curl;
+			return $curl->get($url);
 			
 			// 处理返回的jsonp格式的数据
 			// _Callback({"result":{"code":0,"err":{"code":0},"msg":"","now":1497600398,"num":9,"posts":[{"checkflag":0,}], {..}]..,}})
@@ -53,6 +55,56 @@ class Qzone
 						'content' => $v->content,
 						'created_at' => $v->create_time, 
 					];
+				}
+			}
+		}
+
+		return $ret;
+	}*/
+	public static function getQQTwittees($qq, $page = 1, $count = 10)
+	{
+		$ret = [
+			'code' => 1000,
+			'result' => [],
+		];
+
+		// 整数判断
+		if (ctype_digit(strval($qq))) {
+			$params = [
+				'hostUin' => $qq,
+				'pos' => ($page - 1) * $count,
+				'num' => $count,
+                'ftype' => 0,
+                'sort' => 0,
+                'replynum' => 0,
+                'g_tk' => '',
+                'callbackFun' => '_preloadCallback',
+                'code_version' => 1,
+                'format' => 'json',
+                'need_private_comment' => 1,
+			];
+
+			$url = self::QQ_TWITTEE_URL . '?' . http_build_query($params);
+			$curl = new Curl;
+			$data = $curl->get($url);
+			$data = json_decode($data);
+			if ($data->code > 0) {
+				$ret['code'] = $data->code;
+			} else {
+				$ret['code'] = 0;
+				$data =$data->msglist;
+				foreach ($data as $k => $v) {
+					$temp = [
+						'tid' => $v->tid,
+						'content' => $v->content,
+						'created_at' => $v->created_time,
+					];
+					if (empty($v->content) && $v->rt_title) {
+					    $temp['content'] = $v->rt_title;
+                    }
+					if (!empty($temp['content'])) {
+                        $ret['result'][] = $temp;
+                    }
 				}
 			}
 		}
